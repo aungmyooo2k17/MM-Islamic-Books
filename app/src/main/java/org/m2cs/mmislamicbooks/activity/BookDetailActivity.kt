d7a1822e2e471c9c6d7f44fdc006d680d0727ae1
Aung Myo Oo <dev.aungmyooo2015@gmail.com>
2018-05-31 19:43:53 +0630

    updated main page ui and detail page ui

--- app/src/main/java/org/m2cs/mmislamicbooks/activity/BookDetailActivity.kt
+++ app/src/main/java/org/m2cs/mmislamicbooks/activity/BookDetailActivity.kt
@@ -2,13 +2,21 @@ package org.m2cs.mmislamicbooks.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_book_detail.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import org.m2cs.mmislamicbooks.R
+import org.m2cs.mmislamicbooks.adapter.HomeFragAdapter
 import org.m2cs.mmislamicbooks.model.Books
 
 class BookDetailActivity : AppCompatActivity() {
+
+    val books: ArrayList<Books> = ArrayList()
+    private lateinit var mHomeAdapter: HomeFragAdapter
 
     companion object {
         val BOOK_KEY = "book_key"
@@ -24,9 +32,28 @@ class BookDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
 
+        initializedArray()
+
         val currency = intent.getParcelableExtra<Books>(BOOK_KEY)
 
         author.setText(currency.authorName)
         booktitle.setText(currency.bookName)
+
+        rv_similar_book.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        mHomeAdapter = HomeFragAdapter(this)
+        mHomeAdapter.replaceData(books)
+        rv_similar_book.adapter = mHomeAdapter
+        rv_similar_book.isNestedScrollingEnabled = false
+        rv_similar_book.setFocusable(false);
+
+    }
+
+    private fun initializedArray()
+    {
+        for (i in 1..15)
+        {
+            books.add(Books())
+        }
+
     }
 }
